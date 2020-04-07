979f42d11b5595c25d65d5988de992608c477247
vagrant <vagrant@vagrant.vm>
2020-04-07 10:54:17 -0300

    Customização de resultado de operação

--- src/main/java/com/matera/wcc/calculadora/rest/dto/ResultadoDTO.java
+++ src/main/java/com/matera/wcc/calculadora/rest/dto/ResultadoDTO.java
@@ -1,10 +1,16 @@
 package com.matera.wcc.calculadora.rest.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 
 public class ResultadoDTO {
+    @JsonProperty("primeiro-operando")
     private BigDecimal primeiroOperando;
+
+    @JsonProperty("segundo-operando")
     private BigDecimal segundoOperando;
+
     private BigDecimal resultado;
 
     public BigDecimal getPrimeiroOperando() { return primeiroOperando; }
