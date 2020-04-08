19d0aeadb2d6353e96c818dd04967c87f8eab404
vagrant <vagrant@vagrant.vm>
2020-04-08 14:34:22 -0300

    Inclusao de endpoint rest para historico

---
+++ src/main/java/com/matera/wcc/calculadora/rest/HistoricoOperacaoController.java
@@ -0,0 +1,44 @@
+package com.matera.wcc.calculadora.rest;
+
+import com.matera.wcc.calculadora.domain.Operacao;
+import com.matera.wcc.calculadora.rest.dto.HistoricoOperacaoDTO;
+import com.matera.wcc.calculadora.rest.dto.TipoOperacaoDTO;
+import com.matera.wcc.calculadora.service.HistoricoOperacaoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class HistoricoOperacaoController {
+    private final HistoricoOperacaoService service;
+
+    private HistoricoOperacaoDTO convert(Operacao operacao) {
+        HistoricoOperacaoDTO historicoOperacaoDTO = new HistoricoOperacaoDTO();
+        historicoOperacaoDTO.setId(operacao.getId());
+        historicoOperacaoDTO.setPrimeiroOperando(operacao.getPrimeiroOperando());
+        historicoOperacaoDTO.setSegundoOperando(operacao.getSegundoOperando());
+        historicoOperacaoDTO.setTipoOperacao(TipoOperacaoDTO.valueOf(operacao.getTipoOperacao().name()));
+        historicoOperacaoDTO.setResultado(operacao.getResultado());
+
+        historicoOperacaoDTO.setTimestamp(operacao.getRequisicao().getTimestamp());
+        historicoOperacaoDTO.setIp(operacao.getRequisicao().getIp());
+
+        return historicoOperacaoDTO;
+    }
+
+    public HistoricoOperacaoController(HistoricoOperacaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/historico-operacoes")
+    public List<HistoricoOperacaoDTO> fetchAll() {
+        List <HistoricoOperacaoDTO> operacoes = new ArrayList<>();
+        for (Operacao operacao : this.service.findAll()) {
+            operacoes.add(convert(operacao));
+        }
+        return operacoes;
+    }
+
+}
---
+++ src/main/java/com/matera/wcc/calculadora/rest/dto/HistoricoOperacaoDTO.java
@@ -0,0 +1,71 @@
+package com.matera.wcc.calculadora.rest.dto;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+public class HistoricoOperacaoDTO {
+    private UUID id;
+    private BigDecimal primeiroOperando;
+    private BigDecimal segundoOperando;
+    private BigDecimal resultado;
+    private TipoOperacaoDTO tipoOperacao;
+    private Instant timestamp;
+    private String ip;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public BigDecimal getPrimeiroOperando() {
+        return primeiroOperando;
+    }
+
+    public void setPrimeiroOperando(BigDecimal primeiroOperando) {
+        this.primeiroOperando = primeiroOperando;
+    }
+
+    public BigDecimal getSegundoOperando() {
+        return segundoOperando;
+    }
+
+    public void setSegundoOperando(BigDecimal segundoOperando) {
+        this.segundoOperando = segundoOperando;
+    }
+
+    public BigDecimal getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(BigDecimal resultado) {
+        this.resultado = resultado;
+    }
+
+    public TipoOperacaoDTO getTipoOperacao() {
+        return tipoOperacao;
+    }
+
+    public void setTipoOperacao(TipoOperacaoDTO tipoOperacao) {
+        this.tipoOperacao = tipoOperacao;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}
